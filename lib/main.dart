ddd2e3dafbaf00e7291de6008611e9e582121571
Everton Andrade <evertonfa7@gmail.com>
2021-09-06 00:56:17 -0300

    adiciona firebase para a autenticação

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
+import 'topmenu_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:topmenu_app/services/auth_service.dart';
 
-void main() {
-  runApp(TopMenuApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-class TopMenuApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'TopMenu App',
-      theme: ThemeData(primaryColor: Colors.deepOrange),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Título'),
-        ),
-        body: Center(
-          child: Text('Conteúdo'),
-        ),
-      ),
-    );
-  }
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
+      ],
+      child: TopMenuApp(),
+    ),
+  );
 }
