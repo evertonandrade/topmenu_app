05c52d5f0be5b612f1a80489e8ec820c7b27a8a3
Everton Andrade <evertonfa7@gmail.com>
2021-09-12 19:48:20 -0300

    adiciona bottom navigation na home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:topmenu_app/pages/location_page.dart';
+import 'package:topmenu_app/pages/menu_page.dart';
+import 'package:topmenu_app/pages/settings_page.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -8,14 +11,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentPage = 1;
+  late PageController pc;
+
+  @override
+  void initState() {
+    super.initState();
+    pc = PageController(initialPage: currentPage);
+  }
+
+  setCurrentPage(page) {
+    setState(() {
+      currentPage = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TopMenu App'),
+      body: PageView(
+        controller: pc,
+        children: [
+          LocationPage(),
+          MenuPage(),
+          SettingsPage()
+        ],
+        onPageChanged: setCurrentPage,
       ),
-      body: Center(
-        child: Text('Conteúdo'),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.location_city), label: 'Estabelecimentos'),
+          BottomNavigationBarItem(icon: Icon(Icons.restaurant_menu), label: 'Cardápios'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings_sharp), label: 'Configurações'),
+        ],
+        onTap: (page) {
+          pc.animateToPage(page,
+              duration: Duration(milliseconds: 400), curve: Curves.ease);
+        },
+        backgroundColor: Colors.grey[100],
       ),
     );
   }
